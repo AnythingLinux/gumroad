af5ff47ca4fa12b26d9e572dc410eda75902422c
Sahil Lavingia <sahil.lavingia@gmail.com>
2026-05-28 14:12:09 -0400

    Add auth system tests with Minitest and Playwright

---
+++ test/test_helper.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
+require "rails/test_help"
+
+module ActiveSupport
+  class TestCase
+    self.file_fixture_path = Rails.root.join("spec", "support", "fixtures")
+
+    fixtures_dir = Rails.root.join("test", "fixtures")
+    if fixtures_dir.directory? && Dir[fixtures_dir.join("*.yml")].any?
+      fixtures :all
+    end
+  end
+end
